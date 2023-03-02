81cfd1caa977988c9cc9bf85bb91f15bd8bb593e
LorenzoNiccoli <109612663+LorenzoNiccoli@users.noreply.github.com>
2023-03-02 18:44:13 +0100

    Create HarmonicOscillator_EulerMap.py

---
+++ HarmonicOscillator_EulerMap.py
@@ -0,0 +1,37 @@
+# Armonic oscillator: symplectic and non-symplectic Euler map
+# 
+# Parameters:
+#     x       -   starting position             [m]
+#     p       -   starting momentum             [m/s]
+#     w       -   frequency of the oscillation  [1/s]
+#     dt      -   time step                     [s]
+#     Nsteps  -   number of steps
+#
+# Outputs:
+#     x1n     -   collection of the position over time  [m]
+#     p1n     -   collection of the momenta over time   [m/s]
+#
+# Notes: 
+#     1. This code has been made with a didattical reason in mind, thus it is not the
+#        most perfoning way to solve the problem of the harmonic oscillator. 
+#
+# Author : Lorenzo Niccoli
+# Email  : lorenzo.niccoli@unifi.it (March 2023)
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Functions: Symplectic map (conservative_map), non-symplectic map (nonconservative_map)
+def conservative_map(x,p):
+  
+
+# Starting conditions
+
+# Plot section 
+
+
+
+
+# "Science is a differential equation. Religion is a boundary condition"
+# Alan Turing, epigram to Robin Gandy (1954)
